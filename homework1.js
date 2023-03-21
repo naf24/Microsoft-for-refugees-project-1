7427b9780ca915cffad1a660a6b5d7668dbca90e
elena tertytsia <tertytsia24@gmail.com>
2023-03-21 13:51:32 +0100

    creation base consts

---
+++ homework1.js
@@ -0,0 +1,32 @@
+const basegrey = {
+    material: leather,
+    color: grey,
+};
+const basewhite = {
+    material: leather,
+    color: white,
+};
+const baseblack = {
+    material: leather,
+    color: black,
+};
+const basepink = {
+    material: leather,
+    color: pink,
+};
+const basered = {
+    material: leather,
+    color: red,
+};
+const basebrown = {
+    material: leather,
+    color: brown,
+};
+const baseblue = {
+    material: leather,
+    color: blue,
+};
+const baseyellow = {
+    material: leather,
+    color: yellow,
+};
