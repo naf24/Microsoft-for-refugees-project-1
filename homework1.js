d47231c8d42fc4922dfe47afb92a1a743ca76b2b
elena tertytsia <tertytsia24@gmail.com>
2023-04-07 11:19:07 +0200

    adding numbers of exercises for each part

--- homework1.js
+++ homework1.js
@@ -1,11 +1,27 @@
-const forehead;
-const eyes;
-const nose;
-const cheeks;
-const cheekbones;
-const lips;
-const neck;
-const lift;
+const forehead [
+    exercises amount: 3,
+];
+const eyes [
+    exercises amount: 6,
+];
+const nose [
+    exercises amount: 1,
+];
+const cheeks [
+    exercises amount: 1,
+];
+const cheekbones [
+    exercises amount: 2,
+];
+const lips [
+    exercises amount: 2,
+];
+const neck [
+    exercises amount: 2,
+];
+const lift [
+    exercises amount: 4,
+];
 
 
 
