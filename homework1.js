806ee15c3456421d03d21c0600bbfb8b4ae35bc5
elena tertytsia <tertytsia24@gmail.com>
2023-04-11 10:31:47 +0200

    getting rid of excess consts and lets

--- homework1.js
+++ homework1.js
@@ -1,77 +1,86 @@
-const forehead [
-    exercises amount: 3,
+const forehead [{
+    exercisesAmount: 3,
+}
 ];
-const eyes1 [
-    exercises amount: 3,
+const eyes1 [{
+    exercisesAmount: 3,
+}   
 ];
-const eyes2 [
-    exercises amount: 3,
+const eyes2 [{
+    exercisesAmount: 3,
+} 
 ];
-const nose [
-    exercises amount: 1,
+const nose [{
+    exercisesAmount: 1,
+} 
 ];
-const cheeks [
-    exercises amount: 1,
+const cheeks [{
+    exercisesAmount: 1,
+}   
 ];
-const cheekbones [
-    exercises amount: 2,
+const cheekbones [{
+    exercisesAmount: 2,
+}
 ];
-const lips [
-    exercises amount: 2,
+const lips [{
+    exercisesAmount: 2,
+}  
 ];
-const neck [
-    exercises amount: 2,
+const neck [{
+    exercisesAmount: 2,
+}   
 ];
-const lift [
-    exercises amount: 4,
+const lift [{
+    exercisesAmount: 4,
+} 
 ];
 
 let day = 1;
 
 //4 months access
 
-if (let day <= 7) {
-    console.log(const eyes1, const nose, const lift, const neck);
-} else if (let day => 8 && let day <= 20) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lift, const neck);
-} else if (let day <= 121) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lips, const lift, const neck);
+if (day <= 7) {
+    console.log(eyes1, nose, lift, neck);
+} else if (day => 8 && let day <= 20) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lift, neck);
+} else if (day <= 121) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lips, lift, neck);
 } else {
     console.log("Please make a payment for the further use of the face yoga exercises");
 };
 
 // 6 months access
 
-if (let day <= 7) {
-    console.log(const eyes1, const nose, const lift, const neck);
-} else if (let day >= 8 && let day <= 20) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lift, const neck);
-} else if (let day <= 184) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lips, const lift, const neck);
+if (day <= 7) {
+    console.log(eyes1, nose, lift, neck);
+} else if (day >= 8 && let day <= 20) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lift, neck);
+} else if (day <= 184) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lips, lift, neck);
 } else {
     console.log("Please make a payment for the further use of the face yoga exercises");
 };
 
 // 12 months access
 
-if (let day <= 7) {
-    console.log(const eyes1, const nose, const lift, const neck);
-} else if (let day >= 8 && let day <= 20) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lift, const neck);
-} else if (let day <= 366) {
-    console.log(const eyes1, const eyes2, const forehead, const nose, const cheeks, const cheekbones, const lips, const lift, const neck);
+if (day <= 7) {
+    console.log(eyes1, nose, lift, neck);
+} else if (day >= 8 && day <= 20) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lift, neck);
+} else if (day <= 366) {
+    console.log(eyes1, eyes2, forehead, nose, cheeks, cheekbones, lips, lift, neck);
 } else {
     console.log("Please make a payment for the further use of the face yoga exercises");
 };
 
 //free one week access
 
-if (let day <= 2) {
-    console.log(const eyes1, const nose, const lips);
-} else if (let day <= 4) {
-    console.log(const eyes1, const forehead, const nose, const lips, const lift);
-} else if (let day <= 7) {
-    console.log(const eyes1, const forehead, const nose, const lips, const lift, const neck);    
+if (day <= 2) {
+    console.log(eyes1, nose, lips);
+} else if (day <= 4) {
+    console.log(eyes1, forehead, nose, lips, lift);
+} else if (day <= 7) {
+    console.log(eyes1, forehead, nose, lips, lift, neck);    
 } else {
     console.log("Make a payment to get an access to face yoga program!");
 };
